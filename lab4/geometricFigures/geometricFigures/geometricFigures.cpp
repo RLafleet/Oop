7d517a756fad0355e7a7b3a8c1ba6d116c2f0828
RLafleet <maksim.nazarov@ispring.institute>
2024-04-24 23:00:59 +0300

    Add parser args

--- lab4/geometricFigures/geometricFigures/geometricFigures.cpp
+++ lab4/geometricFigures/geometricFigures/geometricFigures.cpp
@@ -3,18 +3,48 @@
 
 #include <iostream>
 
-int main()
+struct Args 
 {
-    std::cout << "Hello World!\n";
+    std::string inputStr;
+};
+
+Args ParseArgs(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        throw std::invalid_argument("Invalid count of arguments\nUsage: <input_str>");
+    }
+    
+    Args args;
+    args.inputStr = argv[1]; 
+    return args;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+std::ifstream GetFileName(const std::string& inputStr)
+{
+    std::ifstream inputFile(inputStr);
+    return inputFile;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void CheckForOpenFile(std::ifstream& inputFile)
+{
+    if (!inputFile.is_open())
+    {
+        throw std::runtime_error("Failed to open input stream file");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        Args args = ParseArgs(argc, argv);
+        std::cout << args.inputStr << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
