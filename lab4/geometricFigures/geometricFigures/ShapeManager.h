5ebc158e3ad06e5d96b31453c42c85af518cee00
RLafleet <maksim.nazarov@ispring.institute>
2024-04-28 19:13:46 +0300

    Add SFML, add interfaces and classes figures

--- lab4/geometricFigures/geometricFigures/ShapeManager.h
+++ lab4/geometricFigures/geometricFigures/ShapeManager.h
@@ -1,17 +1,17 @@
 #ifndef INC_1_2_SHAPEMANAGER_H
 #define INC_1_2_SHAPEMANAGER_H
 
-#include "../Shape/IShape.h"
-#include "../Shape/CLineSegment/CLineSegment.h"
-#include "../Shape/CRectangle/CRectangle.h"
-#include "../Shape/CCircle/CCircle.h"
-#include "../Shape/CTriangle/CTriangle.h"
-#include "../Factory/ShapeFactory.h"
+#include "IShape.h"
+#include "CLineSegment.h"
+#include "CRectangle.h"
+#include "CCircle.h"
+#include "CTriangle.h"
+#include "ShapeCreator.h"
 
 class ShapeManager
 {
 public:
-	void ConstructShape(const std::string str);
+	void ConstructShape(const std::string& str);
 
 	std::shared_ptr<IShape>GetShapeMaxSize() const;
 
